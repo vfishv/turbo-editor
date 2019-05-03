e14341e1b9dab4a8476c578376ec997f2dffd45b
Quentin <vFISHv@gmail.com>
2019-05-03 21:49:43 +0800

    androidx

--- libraries/sharedCode/src/main/java/sharedcode/turboeditor/dialogfragment/EncodingDialog.java
+++ libraries/sharedCode/src/main/java/sharedcode/turboeditor/dialogfragment/EncodingDialog.java
@@ -23,7 +23,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
+import androidx.appcompat.widget.SwitchCompat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
--- libraries/sharedCode/src/main/java/sharedcode/turboeditor/util/systemui/SystemUiHelperImplHC.java
+++ libraries/sharedCode/src/main/java/sharedcode/turboeditor/util/systemui/SystemUiHelperImplHC.java
@@ -20,8 +20,8 @@
 package sharedcode.turboeditor.util.systemui;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 
